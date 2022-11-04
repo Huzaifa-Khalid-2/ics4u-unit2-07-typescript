e79d0ea52fe7f117f047ec611eeb5b009d6b8a85
Huzaifa-Khalid-2 <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2022-11-04 10:42:11 -0400

    Update Truck.ts

--- src/Truck.ts
+++ src/Truck.ts
@@ -1,85 +1,45 @@
-/**
- * This class creates a vehicle
+/*
+ * This program is a truck class.
  *
- * By:      Huzaifa Khalid
- * Version: 1.0
- * Since:   2022-10-18
+ * @author  Huzaifa Khalid
+ * @version 1.0
+ * @since   2020-11-01
  */
 
-class Vehicle {
-  public speed: number = 0;
-  public maxSpeed: number;
-  public numberOfDoors: number;
-  public licensePlate: string;
-  public colour: string;
+import Vehicle from './Vehicle'
 
-  constructor(
-    colour: string,
-    licensePlate: string,
-    numberOfDoors: number,
-    maxSpeed: number
-  ) {
-    this.licensePlate = licensePlate;
-    this.colour = colour;
-    this.maxSpeed = maxSpeed;
-    this.numberOfDoors = numberOfDoors;
-  }
+class Truck extends Vehicle {
+  private licensePlate: string
 
-  // getters
-  public getSpeed(): number {
-    return this.speed;
-  }
-
-  public getMaxSpeed(): number {
-    return this.maxSpeed;
-  }
-
-  public getNumberOfDoors(): number {
-    return this.numberOfDoors;
-  }
-
-  public getLicensePlate(): string {
-    return this.licensePlate;
-  }
-
-  public getColour(): string {
-    return this.colour;
-  }
-
-  // setters
-  public setLicensePlate(thePlate: string): void {
-    this.licensePlate = thePlate;
-  }
-
-  public setColour(theColour: string): void {
-    this.colour = theColour;
-  }
-
-  public accelerate(accelerationPower: number, accelerationTime: number): void {
-    this.speed = accelerationPower * accelerationTime + this.speed;
-
-    if (this.speed > this.maxSpeed) {
-      this.speed = this.maxSpeed;
-    }
-  }
-
-  public break(breakPower: number, breakTime: number): void {
-    this.speed = this.speed - breakPower * breakTime;
-
-    if (this.speed < 0) {
-      this.speed = 0;
-    }
+  constructor(colour: string, maxSpeed: number, licensePlate: string) {
+    super(colour, maxSpeed)
+    this.licensePlate = licensePlate
   }
 
   public status(): void {
     console.log(
-      `        --> Speed: ${this.speed}
-        --> MaxSpeed: ${this.maxSpeed}
-        --> Number of doors: ${this.numberOfDoors}
+      `        --> Speed: ${super.getSpeed()}
+        --> MaxSpeed: ${super.getMaxSpeed()}
         --> License Plate: ${this.licensePlate}
-        --> Colour: ${this.colour}`
-    );
+        --> Colour: ${super.getColour()}`
+    )
+  }
+
+  public getLicensePlate(): string {
+    return this.licensePlate
+  }
+
+  public setLicensePlate(newLicensePlate: string): void {
+    this.licensePlate = newLicensePlate
+  }
+
+  public provideAir(airPressure: number): void {
+    super.setSpeed(super.getSpeed() - airPressure / 2)
+
+    if (super.getSpeed() < 0) {
+      super.setSpeed(0)
+    }
   }
 }
 
-export = Vehicle;
+export = Truck

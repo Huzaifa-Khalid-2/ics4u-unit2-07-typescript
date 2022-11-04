00b45051d954b01b42593b8ec5028bff753f0776
Huzaifa-Khalid-2 <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2022-11-04 10:41:36 -0400

    Update Bike.ts

---
+++ src/Bike.ts
@@ -0,0 +1,40 @@
+/*
+ * This program is a bike class.
+ *
+ * @author  Huzaifa Khalid
+ * @version 1.0
+ * @since   2020-11-02
+ */
+
+import Vehicle from './Vehicle'
+
+class Bike extends Vehicle {
+  public cadence = 0
+
+  // getter and setter
+  public status(): void {
+    console.log(
+      `        --> Speed: ${super.getSpeed()}
+          --> MaxSpeed: ${super.getMaxSpeed()}
+          --> Color: ${super.getColour()}
+          --> Cadence: ${this.cadence}\n`
+    )
+  }
+
+  public getCadence(): number {
+    return this.cadence
+  }
+
+  // accelerate() method - increases the cadence
+  public accelerate(appliedPower: number): void {
+    this.cadence = this.cadence + appliedPower
+    super.setSpeed(this.cadence * 2)
+  }
+
+  // ringBell() method - prints a bell sound
+  public ringBell(): void {
+    console.log('Ding ding!\n')
+  }
+}
+
+export = Bike

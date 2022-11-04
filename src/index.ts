0b17ee12940b6da33a2180229262ef3cdf1a5263
Huzaifa-Khalid-2 <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2022-11-04 10:43:19 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,45 @@
-/**
- * The program is the classic
- * Vehicle stats program
+/*
+ * This program is a vehicle class.
  *
- * By:      Huzaifa Khalid
- * Version: 1.0
- * Since:   2020-10-17
+ * @author  Huzaifa Khalid
+ * @version 1.0
+ * @since   2020-11-01
  */
 
-import Truck from "./Truck";
-import Bike from "./Bike";
+import Truck from './Truck'
+import Bike from './Bike'
 
-const bmx = new Bike("Red", 40);
+const bmx = new Bike('Red', 40)
 
-console.log("Created Bmx bike.\nStatus:\n");
-bmx.status();
+console.log('Created Bmx bike.\nStatus:\n')
+bmx.status()
 
-console.log("Set the cadense to 10\n");
-bmx.accelerate(10);
-bmx.status();
+console.log('Set the cadense to 10\n')
+bmx.accelerate(10)
+bmx.status()
 
-console.log("\nAccelerate by 15:");
-bmx.accelerate(15);
-bmx.status();
+console.log('\nAccelerate by 15:')
+bmx.accelerate(15)
+bmx.status()
 
-console.log("\nRing bell.");
-bmx.ringBell();
+console.log('\nRing bell.')
+bmx.ringBell()
 
-const bigTruck = new Truck("Grey", "HGC-3456F", 200);
+const bigTruck = new Truck('Grey', 200, 'HGC-3456F')
 
-console.log("Created a Truck.\nStatus:\n");
-bigTruck.status();
+console.log('Created a Truck.\nStatus:\n')
+bigTruck.status()
 
-console.log("\nAccelerating, 10 of power for ten seconds:");
-bigTruck.accelerate(10, 10);
-console.log("New speed: " + String(bigTruck.getSpeed()));
+console.log('\nAccelerating, 10 of power for ten seconds:')
+bigTruck.accelerate(10, 10)
+console.log('New speed: ' + bigTruck.speed)
 
-console.log("\nBreaking, 10 of power for 10 sec.");
-bigTruck.accelerate(10, 10);
-console.log("New speed: " + String(bigTruck.getSpeed()));
+console.log('\nBreaking, 10 of power for 10 sec.')
+bigTruck.accelerate(10, 10)
+console.log('New speed: ' + bigTruck.speed)
 
-console.log("\nApplyed air pressure of 10:");
-bigTruck.provideAir(10);
-console.log("New speed: " + String(bigTruck.getSpeed()));
+console.log('\nApplyed air pressure of 10:')
+bigTruck.provideAir(10)
+console.log('New speed: ' + bigTruck.speed)
 
-console.log("\nDone.");
+console.log('\nDone.')
